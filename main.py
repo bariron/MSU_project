20c2b896d97abac5297e5e9c428c47469dbc615a
nickspb-ui <nickoustinov@gmail.com>
2021-08-24 09:50:07 +0300

    small fixes of reg

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ async def on_ready():
 
 @client.event
 async def on_member_join(member):
-    channel = discord.utils.get(member.guild.text_channels, name="registration")  # ёбаныфй костыль - ПЕРЕДЕЛАТЬ
+    channel = discord.utils.get(member.guild.text_channels, name="registration")
     no_name = discord.utils.get(member.guild.roles, name='no_name')
     await member.add_roles(no_name)
     await channel.send(f'{member.mention} .reg чтобы зарегистрироваться (Фамилия Имя Номер_группы)')
@@ -45,11 +45,8 @@ async def ban(ctx, member: discord.Member, *, reason=None):
 @client.command(pass_context=True)
 async def help(ctx):
     emb = discord.Embed(title='Навигация по командам')
-    emb.add_field(name='{}time'.format("."), value='Показывает Московское время')
-    emb.add_field(name='{}hello'.format("."), value='С вами поздороваються')
     emb.add_field(name='{}reg'.format("."), value='Регистрация')
     await ctx.send(embed=emb)
-
 
 @client.command()
 @commands.has_permissions(administrator=True)
@@ -58,28 +55,32 @@ async def mute(ctx, member: discord.Member):
     mute_role = discord.utils.get(ctx.message.guild.roles, name='mute')
     await member.add_roles(mute_role)
 
-
 @client.command()
 async def reg(ctx, surname=' ', name=' ', group=' '):
+    channel = discord.utils.get(ctx.guild.text_channels, name="registration")
+    if ctx.channel != channel:
+        msg = await channel.fetch_message(ctx.message.id)
+        await msg.delete()
     if (not group.isdigit()) | (surname == ' ') | (name == ' ') | (group == ' '):
         await ctx.send(f'{ctx.author.mention}, введите ".reg Фамилия Имя №Группы" для регистрации.')
-    student = discord.utils.get(ctx.message.guild.roles, name='student')
-    await ctx.author.add_roles(student)
-    await ctx.author.edit(nick='{0} {1}'.format(surname, name))
-    no_name = discord.utils.get(ctx.message.guild.roles, name='no_name')
-    await ctx.author.remove_roles(no_name)
-    if group[0] == '1':
-        k1 = discord.utils.get(ctx.message.guild.roles, name='1 курс')
-        await ctx.author.add_roles(k1)
-    elif group[0] == '2':
-        k2 = discord.utils.get(ctx.message.guild.roles, name='2 курс')
-        await ctx.author.add_roles(k2)
-    elif group[0] == '3':
-        k3 = discord.utils.get(ctx.message.guild.roles, name='3 курс')
-        await ctx.author.add_roles(k3)
-    elif group[0] == '4':
-        k4 = discord.utils.get(ctx.message.guild.roles, name='4 курс')
-        await ctx.author.add_roles(k4)
+    else:
+        student = discord.utils.get(ctx.message.guild.roles, name='student')
+        await ctx.author.add_roles(student)
+        await ctx.author.edit(nick='{0} {1}'.format(surname, name))
+        no_name = discord.utils.get(ctx.message.guild.roles, name='no_name')
+        await ctx.author.remove_roles(no_name)
+        if group[0] == '1':
+            k1 = discord.utils.get(ctx.message.guild.roles, name='1 курс')
+            await ctx.author.add_roles(k1)
+        elif group[0] == '2':
+            k2 = discord.utils.get(ctx.message.guild.roles, name='2 курс')
+            await ctx.author.add_roles(k2)
+        elif group[0] == '3':
+            k3 = discord.utils.get(ctx.message.guild.roles, name='3 курс')
+            await ctx.author.add_roles(k3)
+        elif group[0] == '4':
+            k4 = discord.utils.get(ctx.message.guild.roles, name='4 курс')
+            await ctx.author.add_roles(k4)
 
     # if group.isdigit():
     # добавить роль, обозначающую группу
