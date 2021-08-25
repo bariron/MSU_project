e2fb6156f7c1ea52ad89817dd90588dfba8355ab
nickspb-ui <nickoustinov@gmail.com>
2021-08-25 13:07:49 +0300

    added not-student role

--- main.py
+++ main.py
@@ -76,20 +76,26 @@ async def reg(ctx, surname=' ', name=' ', group=' '):
         await ctx.send(f'{ctx.author.mention}, введите ".reg Фамилия Имя №Группы" для регистрации.')
     else:
         student = discord.utils.get(ctx.message.guild.roles, name='student')
-        await ctx.author.add_roles(student)
         await ctx.author.edit(nick='{0} {1}'.format(surname, name))
         no_name = discord.utils.get(ctx.message.guild.roles, name='no_name')
         await ctx.author.remove_roles(no_name)
+        if group[0] == '0':
+            not_student = discord.utils.get(ctx.message.guild.roles, name='not-student')
+            await ctx.author.add_roles(not_student)
         if group[0] == '1':
+            await ctx.author.add_roles(student)
             k1 = discord.utils.get(ctx.message.guild.roles, name='1 курс')
             await ctx.author.add_roles(k1)
         elif group[0] == '2':
+            await ctx.author.add_roles(student)
             k2 = discord.utils.get(ctx.message.guild.roles, name='2 курс')
             await ctx.author.add_roles(k2)
         elif group[0] == '3':
+            await ctx.author.add_roles(student)
             k3 = discord.utils.get(ctx.message.guild.roles, name='3 курс')
             await ctx.author.add_roles(k3)
         elif group[0] == '4':
+            await ctx.author.add_roles(student)
             k4 = discord.utils.get(ctx.message.guild.roles, name='4 курс')
             await ctx.author.add_roles(k4)
 
